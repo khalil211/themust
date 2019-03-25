75b5e1267dd1e34f06a9baeaf75a21dd2a9a3571
mouhli01 <aziz.mouhligharbi@esprit.tn>
2019-03-25 13:31:13 +0100

    ajout pub php

---
+++ admin/delete.php
@@ -0,0 +1,9 @@
+<?php
+include "../config.php";
+$db=config::getConnexion();
+if (isset($_GET['del'])) {
+	$ID = $_GET['del'];
+	$emps=$db->query("DELETE FROM publicite WHERE ID=$ID");
+	
+	header('location: tables-pub.php');
+}
