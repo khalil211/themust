a5ab484919a47ac878fefe48bf150322cb2c7c3d
Moez6060 <elmoez.amira@esprit.tn>
2019-03-29 00:01:19 +0100

    front/back forms

---
+++ admin/assets/js/inscription.js
@@ -0,0 +1,65 @@
+function surligne(champ, erreur)
+{
+   if(erreur)
+      champ.style.backgroundColor = "#f23d0f";
+   else
+      champ.style.backgroundColor = "";
+}
+
+function verifMail(champ)
+{
+   var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+   if(!regex.test(champ.value))
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifPassword(champ)
+{
+   if(champ.value.length < 6 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifPseudo(champ)
+{
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifForm(f)
+{
+   var identifiantOk = verifPseudo(f.identifiant);
+   var mailOk = verifMail(f.email);
+   var passwordOk = verifPassword(f.password);
+   
+   if(pseudoOk && mailOk && passwordOk)
+      return true;
+   else
+   {
+      alert("Veuillez remplir correctement tous les champs");
+      return false;
+   }
+}
