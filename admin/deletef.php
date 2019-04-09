882a061301c2a836758ccaf017d54a99c6a159a6
mouhli01 <aziz.mouhligharbi@esprit.tn>
2019-04-09 12:38:19 +0100

    fidélité

---
+++ admin/deletef.php
@@ -0,0 +1,9 @@
+<?php
+include "../config.php";
+$db=config::getConnexion();
+if (isset($_GET['del'])) {
+	$ID = $_GET['del'];
+	$emps=$db->query("DELETE FROM fidelite WHERE ID=$ID");
+	
+	header('location: tables-fidélité.php');
+}
